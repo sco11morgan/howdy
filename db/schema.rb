9f655ce8ceb8e8f17ac544088f905197aff60891
Scott Morgan <morgandev@gmail.com>
2024-02-10 09:20:01 -0600

    add some models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_09_181717) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_09_184226) do
+  create_table "posts", force: :cascade do |t|
+    t.text "message"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "name"
@@ -21,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_09_181717) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
