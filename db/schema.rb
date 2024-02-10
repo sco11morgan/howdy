5ea6b12804eb632da2603e59f1520c3b617d1e49
Scott Morgan <morgandev@gmail.com>
2024-02-10 15:28:01 -0600

    add teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_10_170947) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_10_212423) do
   create_table "posts", force: :cascade do |t|
     t.text "message"
     t.integer "user_id", null: false
@@ -21,6 +21,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_10_170947) do
 
   create_table "questions", force: :cascade do |t|
     t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
