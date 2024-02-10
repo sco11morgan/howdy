423d2f41a85d64e0095291f98b0cd68ca6120981
Scott Morgan <morgandev@gmail.com>
2024-02-10 11:55:33 -0600

    setup bootstrap + popper

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,8 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
+pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js'
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+
 pin_all_from "app/javascript/controllers", under: "controllers"
