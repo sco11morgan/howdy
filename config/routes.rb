8df0f591b86849ef69583752059cc2a6a46efe1d
Scott Morgan <morgandev@gmail.com>
2024-02-19 15:53:35 -0600

    render standup

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "user_standups#index"
 end
