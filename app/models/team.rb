48494654bb4a66a9396c5a1b8c9b5d56d9be9ade
Scott Morgan <morgandev@gmail.com>
2024-02-18 20:49:09 -0600

    order team by user name

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,9 @@
 class Team < ApplicationRecord
   has_many :user_teams
   has_many :users, through: :user_teams
+
+  def user_teams_ordered_by_name
+    user_teams.includes(:user).order("users.name")
+  end
+
 end
